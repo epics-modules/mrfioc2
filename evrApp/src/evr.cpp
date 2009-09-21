2ac16d08e9a3d722074e9832ad1afd881be2952f
Michael Davidsaver <mdavidsaver@bnl.gov>
2009-09-21 14:39:51 -0400

    more

--- evrApp/src/evr.cpp
+++ evrApp/src/evr.cpp
@@ -1,18 +1,6 @@
 
 #include "evr.hpp"
 
-EVR::EVR() :
-   model(Type::Invalid)
-  ,version(0)
-  ,inputCount(0)
-  ,outputCount(0)
-  ,prescalerCount(0)
-  ,pulserCount(0)
-  ,fifoPresent(0)
-  ,dataBufPresent(0)
-{
-}
-
 EVR::~EVR()
 {
 }
--- evrApp/src/evr.hpp
+++ evrApp/src/evr.hpp
@@ -2,123 +2,9 @@
 #ifndef EVR_HPP_INC
 #define EVR_HPP_INC
 
-#include <cstdlib>
-
-#include <shareLib.h>
-#include <epicsTypes.h>
-#include <dbScan.h>
-
-/** A general way to notify device support of status changes
- *
- * Use to impliment get_ioint_info()
- */
-class IOStatus
-{
-public:
-  virtual IOSCANPVT statusChange(){return NULL;};
-};
-
-struct TimeUnits {
-  enum type {
-    Tick,
-    Micro,
-    Milli,
-    Sec
-  };
-};
-
-//! Three state logic
-struct TSL {
-  enum type {
-    Float, //!< Driver off
-    Low,
-    High
-  };
-};
-
-struct MapType {
-  enum type {
-    None,
-    Trigger,
-    Reset,
-    Set
-  };
-};
-
-class Pulser : public IOStatus
-{
-public:
-  virtual ~Pulser()=0;
-
-  virtual bool enabled()=0;
-  virtual void enable(bool)=0;
-
-  virtual delay(epicsUInt32)=0;
-  virtual epicsUInt32 setDelay()=0;
-  virtual delayUnit(TimeUnits::type)=0;
-  virtual TimeUnits::type setDelayUnit()=0;
-
-  virtual width(epicsUInt32)=0;
-  virtual epicsUInt32 setWidth()=0;
-  virtual widthUnit(TimeUnits::type)=0;
-  virtual TimeUnits::type setWidthUnit()=0;
-
-  virtual epicsUint32 prescaler()=0;
-  virtual void setPrescaler(epicsUint32)=0;
-
-  virtual bool polarityNorm()=0;
-  virtual void setPolarityNorm(bool)=0;
-
-  // Manually set state
-  virtual TSL::type state()=0;
-  // Settings other than TSL::Float temporarily disable all event mappings
-  virtual void setState(TSL::type)=0;
-
-  /**
-   * Control which source(s) effect this pulser.
-   *
-   * Meaning of source id number is device specific (0 = not mapped).
-   *
-   * Note: this is one place where Device Support will have some depth.
-   */
-  virtual MapType::type mappedSource(epicsUInt32)=0;
-  virtual void sourceSetMap(epicsUInt32,MapType::type)=0;
-  virtual const char* sourceName(epicsUInt32)=0;
-
-  virtual const char* mapDesc(epicsUInt32,MapType::type);//!< Default uses sourceName()
-};
-
-class Output : public IOStatus
-{
-public:
-  virtual ~Output()=0;
-
-  /**
-   * Control which source(s) effect this pulser.
-   *
-   * Meaning of source id number is device specific (0 = not mapped).
-   *
-   * Note: this is one place where Device Support will have some depth.
-   */
-  virtual bool source(epicsUInt32)=0;
-  virtual void setSource(epicsUInt32, bool)=0;
-  virtual const char* sourceName(epicsUInt32)=0;
-
-  // Manually set state
-  virtual TSL::type state()=0;
-  // Settings other than TSL::Float temporarily disable all sources
-  virtual void setState(TSL::type)=0;
-};
-
-class PreScaler : public IOStatus
-{
-public:
-  virtual ~PreScaler()=0;
-
-  virtual epicsUint32 prescaler()=0;
-  virtual void setPrescaler(epicsUint32)=0;
-};
-
+#include "evr/pulser.hpp"
+#include "evr/output.hpp"
+#include "evr/prescaler.hpp"
 
 /**@brief Base interface for EVRs.
  *
@@ -132,7 +18,6 @@ class EVR
 {
 public:
 
-  EVR();
   virtual ~EVR()=0;
 
   //! Hardware model
@@ -141,16 +26,26 @@ public:
   //! Firmware Version
   virtual epicsUInt32 version()=0;
 
+  /**\defgroup ena Enable/disable pulser output.
+   */
+  /*@{*/
   virtual bool enabled()=0;
   virtual void enable(bool)=0;
+  /*@}*/
 
-  /** Conversion between frequency and clock word
+
+  /**\defgroup pll Module reference clock
+   *
+   *  Conversion between frequency and clock word
    *  Should happen outside.
    */
+  /*@{*/
   virtual epicsUInt32 pllClock()=0;
   virtual void pllSetClock(epicsUInt32)=0;
+  //! Test for PLL error conditions
   virtual bool pllLocked()=0;
   virtual IOSCANPVT pllChanged()=0;
+  /*@}*/
 
   //! Pulser id number is device specific
   virtual Pulser* pulser(epicsUInt32)=0;
