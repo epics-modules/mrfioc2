d0b86b40545cb65dc80a3c2f18fc5eb2b77e787d
Michael Davidsaver <mdavidsaver@bnl.gov>
2009-12-04 14:25:57 -0500

    mrmevr: testing IOC

---
+++ iocBoot/iocevrmrm/st.cmd
@@ -0,0 +1,23 @@
+
+#< envPaths
+
+cd("/mnt")
+
+## Register all support components
+dbLoadDatabase("dbd/evrmrm.dbd")
+evrmrm_registerRecordDeviceDriver(pdbbase)
+
+mrmevrSetupAuto()
+
+dbLoadRecords("db/evrbase.db","P=evr:,C=0")
+#dbLoadRecords("db/evrmap.db","P=evr:,M=heartbeat:,C=0,func=7")
+#dbLoadRecords("db/evrmap.db","P=evr:,M=resetscaler1:,C=0,func=8")
+#dbLoadRecords("db/evrmap.db","P=evr:,M=resetscaler2:,C=0,func=8")
+
+dbLoadRecords("db/evrout.db","P=evr:,O=irqmap:,C=0,OT=0,IDX=0")
+dbLoadRecords("db/evrout.db","P=evr:,O=fp1:,C=0,OT=1,IDX=0")
+dbLoadRecords("db/evrout.db","P=evr:,O=fp2:,C=0,OT=1,IDX=1")
+dbLoadRecords("db/evrout.db","P=evr:,O=fp3:,C=0,OT=1,IDX=2")
+
+cd("${TOP}/iocBoot/${IOC}")
+iocInit()
