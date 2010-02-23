9a714956e033ee296e10fb231235dee710b74e57
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-02-23 15:33:26 -0500

    mrmevr: add VME evr to test IOC

--- iocBoot/iocevrmrm/st.cmd
+++ iocBoot/iocevrmrm/st.cmd
@@ -9,7 +9,12 @@ evrmrm_registerRecordDeviceDriver(pdbbase)
 
 mrmEvrSetupPCI(0,1,2,0)
 
-dbLoadRecords("db/evr-pmc-230.db","P=evr:,C=0")
+bspExtVerbosity=0
+mrmEvrSetupVME(1,3,0x100000)
+
+dbLoadRecords("db/evr-pmc-230.db","P=evr1:,C=0")
+
+dbLoadRecords("db/evr-vmerf-230.db","P=evr2:,C=1")
 
 cd("${TOP}/iocBoot/${IOC}")
 iocInit()
