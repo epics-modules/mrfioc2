922278559b68a89962d8c5273e99c79f1d4b0cab
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-02-24 15:21:40 -0500

    vme: allow devLib VME apps to link on Linux

---
+++ vmeApp/src/devlib_compat.c
@@ -0,0 +1,24 @@
+
+#define epicsExportSharedSymbols
+#include <shareLib.h>
+
+/*
+ * Most devlib function go through an indirection table with a null
+ * implimentation provided for systems which doen't impliment some
+ * functionality.  However, the functions below don't use this table.
+ * Provide a null implimentation so that devLib applications will
+ * still link on these systems.
+ */
+
+epicsShareFunc long devEnableInterruptLevelVME (unsigned vectorNumber)
+{
+  return -1;
+}
+
+epicsShareFunc long devConnectInterruptVME(
+                        unsigned vectorNumber,
+                        void (*pFunction)(void *),
+                        void  *parameter)
+{
+  return -1;
+}
