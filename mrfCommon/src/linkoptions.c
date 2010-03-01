f1877dae04fbe97ad7e5c8f931db5ff53ea57025
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-03-01 12:04:16 -0500

    common: linkoption for INST_IO parsing and storage

---
+++ mrfCommon/src/linkoptions.c
@@ -0,0 +1,207 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <math.h>
+
+#include <dbDefs.h>
+#include <epicsTypes.h>
+#include <epicsString.h>
+#include <macLib.h>
+
+#define epicsExportSharedSymbols
+#include "linkoptions.h"
+
+/*
+ * Parse the value 'val' according to the type named in 'opt->optType'
+ * and store the result at 'user + opt->offset'.
+ */
+static
+int
+store_value(const linkOptionDef* opt, void* user, const char* val, int options)
+{
+    epicsUInt32 *ival;
+    int *eval;
+    const linkOptionEnumType *emap;
+    double *dval;
+    char *sval;
+    char *end;
+
+    switch(opt->optType) {
+    case linkOptionInt32:
+        if (opt->size<sizeof(epicsUInt32)) {
+              fprintf(stderr, "Provide storage (%d bytes) is too small for Int32 (%d)\n",
+                              opt->size, sizeof(epicsUInt32));
+              return -1;
+        }
+        ival=(epicsUInt32*)( (char*)user + opt->offset );
+
+        *ival = strtoul(val, &end, 0);
+        /* test for the myriad error conditions which strtol may use */
+        if ( (errno==ERANGE && (*ival==LONG_MAX || *ival==LONG_MIN)) ||
+             (errno!=0 && *ival==0) ||
+             (end==val) )
+        {
+            fprintf(stderr,"value %s can't be converted for integer key %s\n",val,opt->name);
+            return -1;
+        }
+        break;
+    case linkOptionDouble:
+        if (opt->size<sizeof(double)) {
+              fprintf(stderr, "Provide storage (%d bytes) is too small for double (%d)\n",
+                              opt->size, sizeof(double));
+              return -1;
+        }
+        dval=(double*)( (char*)user + opt->offset );
+
+        *dval = strtod(val, &end);
+        /* Indicates errors in the same manner as strtol */
+        if ( (errno==ERANGE && *dval==HUGE_VAL) ||
+             (errno!=0 && *dval==0) ||
+             (end==val) )
+        {
+            fprintf(stderr,"value %s can't be converted for double key %s\n",val,opt->name);
+            return -1;
+        }
+        break;
+    case linkOptionEnum:
+        if (opt->size<sizeof(int)) {
+              fprintf(stderr, "Provide storage (%d bytes) is too small for enum (%d)\n",
+                              opt->size, sizeof(int));
+              return -1;
+        }
+        eval=(int*)( (char*)user + opt->offset );
+
+        for(emap=opt->Enums; emap && emap->name; emap++) {
+            if ( strcmp(emap->name, val)==0 ) {
+                *eval=emap->value;
+                break;
+            }
+        }
+
+        if( !emap || !emap->name ) {
+            fprintf(stderr,"%s is not a valid value for the Enum %s\n",
+                   val, opt->name);
+            return -1;
+        }
+
+        break;
+    case linkOptionString:
+        if (opt->size<sizeof(char*)) {
+              /* Catch if someone has given us a char* instead of a char[]
+               * Also means that char buffers must be >4.
+               */
+              fprintf(stderr, "Provide storage (%d bytes) is too small for string (>= %d)\n",
+                              opt->size, sizeof(char*));
+              return -1;
+        }
+        sval=( (char*)user + opt->offset );
+
+        strncpy(sval, val, opt->size-1);
+        sval[opt->size-1]='\0';
+        break;
+    case linkOptionInvalid:
+        fprintf(stderr,"Can't store \'%s\' for %s as the storage type is not defined\n",
+               val,opt->name);
+        return -1;
+        break;
+    }
+
+    return 0;
+}
+
+int
+epicsShareAPI
+linkOptionsStore(const linkOptionDef* opts, void* user, const char* str, int options)
+{
+    MAC_HANDLE handle; /* only .debug is used */
+
+    int status=0;
+    size_t i;
+    epicsUInt32 *found;
+    const linkOptionDef *cur;
+    char **pairs=NULL, **arg;
+
+    for(i=0, cur=opts; cur; i++, cur++) {}
+
+    /* Bit array to find missing required keys */
+    found=calloc( (i/32)+1, sizeof(epicsUInt32) );
+    if (!found) {
+        fprintf(stderr,"store_options: calloc failed\n");
+        status=-1;
+        goto errbitarray;
+    }
+
+    memset((void*)&handle, 0, sizeof(handle));
+
+    if (options&LINKOPTIONDEBUG)
+        handle.debug=0xff;
+
+    /* Parses 'str' and stores the result in the array pairs.
+     * The length of pairs is twice the number of pairs found.
+     * Each even element is a key, each odd element is a value.
+     *
+     * Pair N is (pairs[2*N], pairs[2*N+1])
+     *
+     * After the last pair is a (NULL, NULL)
+     */
+    if (macParseDefns(&handle, str, &pairs)<0) {
+        status=-1;
+        goto errparse;
+    }
+
+    for (i=0, cur=opts; cur && cur->name; i++, cur++) {
+
+        if (options&LINKOPTIONDEBUG)
+            fprintf(stderr,"For option: %s\n",cur->name);
+
+        for (arg=pairs; arg && arg[0]; arg+=2) {
+
+            if (options&LINKOPTIONDEBUG)
+                printf("key %s\n",arg[0]);
+
+            if( strcmp(arg[0], cur->name)!=0 )
+                continue;
+
+            if (found[i/32]&(1<<(i%32)) && !cur->overwrite) {
+                fprintf(stderr,"Option %s was already given\n",cur->name);
+                status=-1;
+                goto errsemantix;
+            }
+
+            found[i/32] |= 1<<(i%32);
+
+            status=store_value(cur, user, arg[1], options);
+            if (status)
+                goto errsemantix;
+        }
+
+        if ( !(found[i/32]&(1<<(i%32))) && cur->required ) {
+            fprintf(stderr,"Missing required option %s\n",cur->name);
+            status=-1;
+            goto errsemantix;
+        }
+    }
+
+errsemantix:
+    free((void*)pairs);
+errparse:
+    free(found);
+errbitarray:
+    return status;
+}
+
+epicsShareFunc
+const char*
+epicsShareAPI
+linkOptionsEnumString(const linkOptionEnumType *emap, int i, const char* def)
+{
+    for(; emap && emap->name; emap++) {
+        if ( i == emap->value ) {
+            return emap->name;
+        }
+    }
+    return def;
+}
---
+++ mrfCommon/src/linkoptions.h
@@ -0,0 +1,140 @@
+#ifndef LINKOPTIONS_H
+#define LINKOPTIONS_H
+
+#include <shareLib.h>
+#include <dbDefs.h>
+#include <epicsTypes.h>
+
+/**@file linkoptions.h
+ *@brief Hardware link parsing and storage
+ *
+ * Utility to parse a macLib style string ('key=val, key2=val2')
+ * and store the results directly into a user supplied struture
+ * after appropriate type conversion.
+ *
+ * Before calling linkOptionsStore() a linkOptionDef structure
+ * must be defined for the user structure.
+ *
+ * For example:
+ *
+ * typedef struct myStruct {
+ *   ... other stuff
+ *   epicsUInt32 ival;
+ *   double      dval;
+ *   epicsUInt32 ival2;
+ *   int         enumval;
+ *   char       strval[20];
+ * } myStruct;
+ *
+ * static const
+ * linkOptionEnumType colorEnum[] = { {"Red",1}, {"Green",2}, {"Blue",3}, {NULL,0} };
+ *
+ * static const
+ * linkOptionDef myStructDef[] = {
+ *   linkInt32 (myStruct, ival,    "Integer" , 0, 0),
+ *   linkInt32 (myStruct, ival2,   "Second"  , 1, 0),
+ *   linkDouble(myStruct, dval,    "Double"  , 1, 0),
+ *   linkString(myStruct, strval , "String"  , 1, 0),
+ *   linkEnum  (myStruct, enumval, "Color"   , 1, 0, colorEnum),
+ *   linkOptionEnd
+ * };
+ *
+ * Note: The 4th argument is required (1) or optional (0), the 5th
+ *       whether later definitions override previous ones (1), or
+ *       are an error (0).
+ *
+ * void someFunc(const char *arg) {
+ *   myStruct mine;
+ *
+ *   memset(&mine, 0, sizeof(myStruct)); // set defaults
+ *
+ *   if (linkOptionsStore(myStructDef, &mine, arg, 0))
+ *     goto error;
+ *
+ *   printf("Second=%d\n",mine->ival2);
+ * }
+ *
+ * Would parse the string 'Second=17, Color=Green, String=Hello world, Double=4.2'
+ * and assign ival2=17, dval=4.2, enumval=2, strval="Hello world".  ival is not
+ * required and since 'Integer' was not specified would not remain unchanged.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum linkOption {
+    linkOptionInvalid=0,
+    linkOptionInt32,
+    linkOptionDouble,
+    linkOptionString,
+    linkOptionEnum
+} linkOption;
+
+typedef struct linkOptionEnumType {
+    const char *name;
+    const int value;
+} linkOptionEnumType;
+
+typedef struct linkOptionDef {
+    const char * name;
+    linkOption optType;
+    int required:1;
+    int overwrite:1;
+    epicsUInt32 offset;
+    epicsUInt32 size;
+    const linkOptionEnumType *Enums;
+} linkOptionDef;
+
+#define linkInt32(Struct, Member, Name, Req, Over) \
+{Name, linkOptionInt32, Req, Over, OFFSET(Struct, Member), sizeof( ((Struct*)0)->Member ), NULL}
+
+#define linkDouble(Struct, Member, Name, Req, Over) \
+{Name, linkOptionDouble, Req, Over, OFFSET(Struct, Member), sizeof( ((Struct*)0)->Member ), NULL}
+
+#define linkString(Struct, Member, Name, Req, Over) \
+{Name, linkOptionString, Req, Over, OFFSET(Struct, Member), sizeof( ((Struct*)0)->Member ), NULL}
+
+#define linkEnum(Struct, Member, Name, Req, Over, Enums) \
+{Name, linkOptionEnum, Req, Over, OFFSET(Struct, Member), sizeof( ((Struct*)0)->Member ), Enums}
+
+#define linkOptionEnd {0}
+
+/* Extra output when parsing and converting */
+#define LINKOPTIONDEBUG 1
+
+/**@brief Parse a string a store the result
+ *
+ * Takes the string 'str', parses it according to 'opts', then sorts the
+ * result in 'user'.
+ *
+ *@param opts A null-terminated array of options.
+ *@param user Pointer to a structure whos member offsets are given in 'opts'
+ *@param str  The string to parse
+ *@param options Some modifiers for the parsing process or 0.
+ *               The only option is LINKOPTIONDEBUG.
+ *@return 0 Ok
+ *@return -1 Fail ('user' may be partially modified)
+ */
+epicsShareFunc
+int
+epicsShareAPI
+linkOptionsStore(const linkOptionDef* opts, void* user, const char* str, int options);
+
+/**@brief Return the string associated with Enum 'i'
+ *
+ *@param Enums A null-terminated array of string/integer pairs
+ *@param i An Enum index
+ *@param def String to be returned in 'i' isn't a valid Enum index.
+ *@return A constant string
+ */
+epicsShareFunc
+const char*
+epicsShareAPI
+linkOptionsEnumString(const linkOptionEnumType *Enums, int i, const char* def);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LINKOPTIONS_H */
