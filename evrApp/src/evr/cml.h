c5ced08b9292170b4286e2566ab8be4e39ae4317
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-06-11 10:19:53 -0400

    evr: extend CML interface for new features
    Add calls for frequency and pattern modes
    dset helpers for enum types

--- evrApp/src/evr/cml.h
+++ evrApp/src/evr/cml.h
@@ -6,6 +6,13 @@
 
 #include <epicsTypes.h>
 
+enum cmlMode {
+  cmlModeOrig,
+  cmlModeFreq,
+  cmlModePattern,
+  cmlModeInvalid
+};
+
 enum cmlShort {
   cmlShortRise=0,
   cmlShortHigh,
@@ -13,15 +20,13 @@ enum cmlShort {
   cmlShortLow
 };
 
-/**@brief Configuration for the short (20bit x 4) CML patterns
- */
 class CML : public IOStatus
 {
 public:
   virtual ~CML()=0;
 
-  virtual epicsUInt32 pattern(cmlShort) const=0;
-  virtual void patternSet(cmlShort, epicsUInt32)=0;
+  virtual cmlMode mode() const=0;
+  virtual void setMode(cmlMode)=0;
 
   virtual bool enabled() const=0;
   virtual void enable(bool)=0;
@@ -32,6 +37,36 @@ public:
   virtual bool powered() const=0;
   virtual void power(bool)=0;
 
+  // For original (20bit x 4) mode
+
+  virtual epicsUInt32 pattern(cmlShort) const=0;
+  virtual void patternSet(cmlShort, epicsUInt32)=0;
+
+  // For Frequency mode
+
+  //! Trigger level
+  virtual bool polarityInvert() const=0;
+  virtual void setPolarityInvert(bool)=0;
+
+  virtual epicsUInt32 countHigh() const=0;
+  virtual epicsUInt32 countLow () const=0;
+  virtual void setCountHigh(epicsUInt32)=0;
+  virtual void setCountLow (epicsUInt32)=0;
+
+  // For Pattern mode (20bit x 2048) mode
+  /* Patterns are given as an array of bytes.
+   * each byte represents one bit in the result.
+   * Lengths are given in bytes, but should be a
+   * multiple of 20.
+   */
+
+  virtual epicsUInt32 lenPattern() const=0;
+  virtual epicsUInt32 lenPatternMax() const=0;
+  virtual void getPattern(unsigned char*, epicsUInt32*) const=0;
+  virtual void setPattern(const unsigned char*, epicsUInt32)=0;
+
+  // Helpers
+
   template<cmlShort I>
   epicsUInt32
   getPattern() const{return this->pattern(I);};
@@ -39,6 +74,9 @@ public:
   template<cmlShort I>
   void
   setPattern(epicsUInt32 v){return this->patternSet(I,v);};
+
+  void setModRaw(epicsUInt16 r){setMode((cmlMode)r);};
+  epicsUInt16 modeRaw() const{return (epicsUInt16)mode();};
 };
 
 #endif // CMLSHORT_HPP_INC
