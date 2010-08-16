cdd1b6f4cdf19ac8e80e32188ffa3b20061d7a45
Michael Davidsaver <mdavidsaver@bnl.gov>
2010-08-16 14:57:34 -0400

    drop pciApp and vmeApp

--- pciApp/src/os/default/devLibPCIOSD.c
+++
@@ -1,7 +0,0 @@
-#include <epicsExport.h>
-
-/* Only exists so that this registrar will be present for all targets */
-void devLibPCIRegisterBaseDefault(void)
-{
-}
-epicsExportRegistrar(devLibPCIRegisterBaseDefault);
