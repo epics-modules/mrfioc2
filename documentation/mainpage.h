f9ee0275d36773381799c34de2cc4c02a7289898
Michael Davidsaver <mdavidsaver@bnl.gov>
2011-06-03 14:24:47 -0400

    sf website
    generate via doxygen

---
+++ documentation/mainpage.h
@@ -0,0 +1,70 @@
+#ifndef MAINPAGE_H
+#define MAINPAGE_H
+
+// No code.  Just Doxygen documentation
+
+/**
+
+@mainpage mrfioc2 Timing System Driver for MRF products
+
+@section whatisit What is it?
+
+A driver for VME and PCI cards from Micro Research Finland for implementing a distributed
+timing system.
+
+@section whereis Source
+
+Releases can be found at @url http://sourceforge.net/projects/epics/files/mrfioc2/
+
+This module is versioned with Mercurial and can be viewed at
+@url http://epics.hg.sourceforge.net/hgweb/epics/mrfioc2/
+
+Or checked out with
+
+hg clone http://epics.hg.sourceforge.net:8000/hgroot/epics/mrfioc2
+
+The canonical version of this page is @url http://epics.sourceforge.net/mrfioc2/
+
+@subsection requires Requires
+
+EPICS Base >= 3.14.8.2
+
+@url http://www.aps.anl.gov/epics/
+
+MSI (Macro expension tool)
+
+@url http://www.aps.anl.gov/epics/extensions/msi/index.php
+
+devLib2
+
+@url http://epics.sourceforge.net/devlib2/
+
+@section hardware Supported Hardware
+
+Event Generators.  Current only the VME-EVG-230
+
+Event Receivers.  VME-EVR-230RF, PMC-EVR-230, cPCI-EVR-230, cPCI-EVRTG-300
+
+@note Support for the VME-EVR-230 (non-RF) is present, but has not been tested.
+
+@section doc Documentation
+
+User documentation can be found in in for form of usage manuals for both the
+<a href="evr-usage.pdf">Receiver</a>
+and
+<a href="evg-usage.pdf">Generator</a>
+
+Those interested in the implementation for the Receiver might wish to start with mrmEvrSetupPCI()
+and mrmEvrSetupVME() or the ::EVRMRM class.
+
+For the generator see mrmEvgSetupVME() or the ::evgMrm class.
+
+@author Michael Davidsaver <mdavidsaver@bnl.gov>
+
+@author Jayesh Shah <jshah@bnl.gov>
+
+@author Eric Björklund <bjorklund@lanl.gov>
+
+*/
+
+#endif // MAINPAGE_H
