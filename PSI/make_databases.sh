a476fe45e8778d3486fe0573f95523dc0b08c14b
Blaz Kranjc <blaz.kranjc@cosylab.com>
2015-01-09 16:45:50 +0100

    Added append to list support to PSI/make_databases.sh and additional sanity check

--- PSI/make_databases.sh
+++ PSI/make_databases.sh
@@ -24,19 +24,29 @@ while getopts ":t:o:" o; do
             ;;
         *)
             usage
+            exit 0
             ;;
     esac
 done
 shift $((OPTIND-1))
 
+if [ ! -d "$MRFIOC2_DIR/evrMrmApp" ]; then
+    usage
+    exit 1
+fi
+
+for arg in $@; do
+    DB_LIST+=("$arg")
+done
+
 mkdir -p $OUTPUT_DIR
 for db in ${DB_LIST[@]}; do
     db_name=`basename "$db" | cut -d. -f1`
-    db_file=$(find $MRFIOC2_DIR/*MrmApp/$DB_REL_PATH -name $db_name.db)
+    db_file=$(find $MRFIOC2_DIR/*MrmApp/$DB_REL_PATH -name $db_name.db 2>/dev/null)
     if [ ! -z $db_file ]; then
         cp $db_file $OUTPUT_DIR
     else
-        sub_file=$(find $MRFIOC2_DIR/*MrmApp/$DB_REL_PATH -name $db_name.substitutions)
+        sub_file=$(find $MRFIOC2_DIR/*MrmApp/$DB_REL_PATH -name $db_name.substitutions 2>/dev/null)
         if [ ! -z $sub_file ]; then
             msi -I$(dirname $sub_file) -S$sub_file > "$OUTPUT_DIR/$db_name.db"
 	else
