dcc74d910500cf29e9378663b516b11d03aea49e
Blaz Kranjc <blaz.kranjc@cosylab.com>
2015-01-09 16:29:06 +0100

    Added bash script that copies PSI DB's

--- PSI/make_databases.sh
+++ PSI/make_databases.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
+set -o errexit
 
-OUTPUT_DIR="db"
-MRFIOC2_DIR=..
+OUTPUT_DIR="./db"
+MRFIOC2_DIR=".."
+DB_LIST=(evgSoftSeq.db evg-vme.db evrEvent.db evr-vmerf230.db)
+DB_REL_PATH="Db/PSI"
 
+usage()
+{
+    echo "Usage: ./$0 [options]"
+    echo "Options:"
+    echo "    -t <top dir> (default ..)"
+    echo "    -o <output dir> (default ./db)"
+}
 
+while getopts ":t:o:" o; do
+    case "${o}" in
+        t)
+            MRFIOC2_DIR=${OPTARG}
+            ;;
+        o)
+            OUTPUT_DIR=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
-
+mkdir -p $OUTPUT_DIR
+for db in ${DB_LIST[@]}; do
+    db_name=`basename "$db" | cut -d. -f1`
+    db_file=$(find $MRFIOC2_DIR/*MrmApp/$DB_REL_PATH -name $db_name.db)
+    if [ ! -z $db_file ]; then
+        cp $db_file $OUTPUT_DIR
+    else
+        sub_file=$(find $MRFIOC2_DIR/*MrmApp/$DB_REL_PATH -name $db_name.substitutions)
+        if [ ! -z $sub_file ]; then
+            msi -I$(dirname $sub_file) -S$sub_file > "$OUTPUT_DIR/$db_name.db"
+	else
+            echo "$db not found"
+	fi
+    fi
+done
