0cdcde317e77a73f3b7504288de308d19960578b
Jure Krasna <ext-krasna_j@pc7824.psi.ch>
2015-03-02 10:00:20 +0100

    Multiple fixes to make the application work on the PPMAC platform.
     - Fixed void pointer arithmetic by casting them to epicsUInt8.
     - Added substitution file for the PCIe-EVR-300 card.
     - Fixed kernel driver so that it works on big-endian platforms as well.
     - Updated iocsh function for PCI card registration so that it also includes the PCI address domain number as a parameter.

--- mrmShared/linux/mrf.h
+++ mrmShared/linux/mrf.h
@@ -91,6 +91,7 @@
     INTCSR9056_DM0_Status|INTCSR9056_DM1_Status)
 
 /* For MRM EVR 230 and 300 series
+ * FPGA register and flag values
  */
 
 #define CTRLMain    0x004
@@ -113,8 +114,15 @@
 
 #define IRQ_Enable_ALL  (IRQ_Enable|IRQ_PCIee)
 
+/* The expected value (form factor and 0x1 for EVR) that is
+ * stored in FPGA of EVR300 @ first byte of FPGAVersion register.
+ *
+ * This value is used to test enianness of the EVR 300 PCIe card.
+ */
+#define FPGAVER_EVR300  0x17
+
 #define FPGAVersion 0x02c
-#  define FPGAVer_FF    0x0f000000
+#  define FPGAVer_FF    0xff000000
 
 /* driver private struct */
 
