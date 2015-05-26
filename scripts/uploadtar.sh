0824dc3328d54d5eeea5b140f907a12bfc4215bf
Michael Davidsaver <mdavidsaver@bnl.gov>
2015-05-25 21:29:26 -0400

    add release helper scripts

---
+++ scripts/uploadtar.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+die() {
+    echo "$1"
+    exit 1
+}
+
+[ -f "$1" ] || die "usage $0 <tarfile>"
+
+[ "$USER" ] || die "USER not set"
+
+scp "$1" $USER,epics@frs.sourceforge.net:/home/frs/project/epics/mrfioc2/
