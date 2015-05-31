45a582845c2471b08918516beda383fd794ac6df
Jure Krasna <jure.krasna@cosylab.com>
2015-05-31 18:05:17 +0200

    Changed code to remove the 'type qualifiers ignored on function return type' warning.

--- evgMrmApp/src/evgSequencer/evgSeqRam.h
+++ evgMrmApp/src/evgSequencer/evgSeqRam.h
@@ -17,7 +17,14 @@ public:
     evgSeqRam(const epicsUInt32, evgMrm* const);
     ~evgSeqRam();
 
-    const epicsUInt32 getId() const{return m_id;}
+    /**
+     * Removing first const modifier since the value is copied and
+     * modifiable in any case. The first const is superfluous in this
+     * case and also causes a warning.
+     *
+     * Change by: jkrasna
+     */
+    epicsUInt32 getId() const{return m_id;}
 
     void setEventCode(const std::vector<epicsUInt8>&);
     std::vector<epicsUInt8> getEventCode();
