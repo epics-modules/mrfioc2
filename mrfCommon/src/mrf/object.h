6af6dffe0bd4634cdd12f7038b90f8d7089c6358
Michael Davidsaver <mdavidsaver@gmail.com>
2020-05-26 09:06:19 -0700

    ObjectInst ctor forwarding

--- mrfCommon/src/mrf/object.h
+++ mrfCommon/src/mrf/object.h
@@ -465,7 +465,7 @@ public:
 protected:
     explicit ObjectInst(const std::string& n) : Base(n) {}
     template<typename A>
-    ObjectInst(const std::string& n, A a) : Base(n, a) {}
+    ObjectInst(const std::string& n, A& a) : Base(n, a) {initObject();}
     virtual ~ObjectInst(){}
 public:
 
