b06204491f7e28f567a1e3e46ac87318e3a16811
Gabriel Fedel <gabriel.fedel@esss.se>
2024-01-24 09:35:54 +0100

    fix: updating pushdoc.sh
    To work with github pages. Also changes the generation of the
    evg-usage pdf to use pdflatex.

--- scripts/pushdoc.sh
+++ scripts/pushdoc.sh
@@ -13,10 +13,16 @@ die() {
 builddoc() {
   doxygen
   lyx -batch -e pdf evr-usage.lyx
-  lyx -batch -e pdf evg-usage.lyx
+  lyx -batch -e pdflatex evg-usage.lyx
+  pdflatex evg-usage
   mv evg-usage.pdf evr-usage.pdf html/
 }
 
 (cd documentation && builddoc)
 
-rsync -av --delete "$@" documentation/html/ $USER,epics@frs.sourceforge.net:/home/project-web/epics/htdocs/mrfioc2/
+git checkout gh-pages
+cp -r documentation/html/* .
+rm -rf documentation
+git add .
+git commit -m "Last updates to documentation"
+git push origin gh-pages
