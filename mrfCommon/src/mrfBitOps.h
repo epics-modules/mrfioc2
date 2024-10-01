ae841e0d2d11887924519c246a67ebca88f5b665
Jerzy Jamroz <jerzy.jamroz@ess.eu>
2024-10-01 11:17:45 +0200

    fix: Print 16 bit registers as it is.

--- mrfCommon/src/mrfBitOps.h
+++ mrfCommon/src/mrfBitOps.h
@@ -32,4 +32,7 @@
 #define BITFLIP(ord,len,base,offset,mask) \
         ord ## _WRITE ## len(base,offset, (ord ## _READ ## len(base,offset) ^ (epicsUInt ## len)(mask)))
 
+// Get U16 address
+#define U16_REG(base, no) (base + (2 * (no)))
+
 #endif /* MRFBITOPS_H */
